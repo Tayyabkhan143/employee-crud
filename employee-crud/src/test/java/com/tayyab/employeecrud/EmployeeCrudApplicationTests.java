41a121b70ffa699cf4e30633dddd1a451234c28b
Lenovo <Lenovo@DESKTOP-996U323>
2021-05-16 17:03:09 +0530

    added uat profile

--- employee-crud/src/test/java/com/tayyab/employeecrud/EmployeeCrudApplicationTests.java
+++ employee-crud/src/test/java/com/tayyab/employeecrud/EmployeeCrudApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmployeeCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
